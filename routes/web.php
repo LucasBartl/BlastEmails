02f0f5f93870fa5f69787f5066fc2aa9668db912
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-05-22 15:24:51 -0300

    Iniciando projeto de emails

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\Http\Controllers\NewPasswordController;
+use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
+
+Route::view('/', 'welcome')->name('home');
+
+
+Route::middleware('guest')->group(function () {
+    
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('register', [RegisteredUserController::class, 'store'])->name('register.store');
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+    
+
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+});
+
+require __DIR__ . '/settings.php';
