3af63a3b1d0bc9169a38a220a3988a4645b4832c
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-06-01 15:23:44 -0300

    Setup inicial das abas

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -37,37 +37,104 @@ class CampaignController extends Controller
 
         return back()->with('message', __('Campaing sussccefully restored!'));
     }
-    public function create(String $tab = null)
+    public function create(?String $tab = null)
     {
-        
+
+        //session()->forget('campaing::create');   -> matar sessão 
+
         return view('campaigns.create', [
             'tab' => $tab,
 
-            'form' => match($tab){
+            'form' => match ($tab) {
                 'template' => '_template',
                 'schedule' => '_schedule',
                 default => '_config'
-            }
+            },
+            'data' => session()->get('campaign::create', [
+                'name' => null,
+                'subject' => null,
+                'email_list_id' => null,
+                'template_id' => null,
+                'body' => null,
+                'track_click' => null,
+                'track_open' => null,
+                'sent_at' => null
+            ])
+
         ]);
     }
-    public function store(String $tab = null)
+    public function store(?String $tab = null)
     {
+        $toRoute = '';
+
+
+        $map = array_merge([
+            'name' => null,
+            'subject' => null,
+            'email_list_id' => null,
+            'template_id' => null,
+            'body' => null,
+            'track_click' => null,
+            'track_open' => null,
+            'sent_at' => null
+        ], request()->all());
+
+
+
 
         if (blank($tab)) { // Esta vindo do /create = primeira aba (setup)
 
-            $data = request()->validate([
+            //Validações
+            request()->validate([
                 'name' => ['required', 'max:255'],
                 'subject' => ['required', 'max:40'],
                 'email_list_id' => ['nullable'],
-                'template_id' => ['nullable']
+                'template_id' => ['nullable'],
+                'body' => ['nullable'],
+                'track_click' => ['nullable'],
+                'track_open' => ['nullable'],
+                'sent_at' => ['nullable'],
             ]);
 
-
-            //criando uma sessão e adicionando os dados validados 
-            session()->put('campaing::create', $data);
-            
             //Assim que os dados forem validados vai ser enviado para a proxima pagina necessaria Template, e com isso passamos o tab
-            return to_route('campaigns.create', ['tab' => 'template']);
+            $toRoute =  route('campaigns.create', ['tab' => 'template']);
         }
+
+        if ($tab == 'template') {
+            //Validações
+            request()->validate([
+                'body' => ['required'],
+            ]);
+            $toRoute = route('campaigns.create', ['tab' => 'schedule']);
+        }
+        if ($tab == 'schedule') {
+            //Validações
+            request()->validate([
+                'sent_at' => ['date'],
+            ]);
+            $toRoute = route('campaigns.index');
+        }
+
+        $session = session('campaign::create', [
+            'name' => null,
+            'subject' => null,
+            'email_list_id' => null,
+            'template_id' => null,
+            'body' => null,
+            'track_click' => null,
+            'track_open' => null,
+            'sent_at' => null,
+        ]);
+
+        foreach ($session as $key => $_) {
+            $newValue = data_get($map, $key);
+
+            if (filled($newValue)) {
+                $session[$key] = $newValue;
+            }
+        }
+
+        session()->put('campaign::create', $session);
+        return redirect($toRoute);
     }
 }
--- resources/views/campaigns/create/_template.blade.php
+++ resources/views/campaigns/create/_template.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <x-input-richtext name="body" :value="old('body')"></x-input-richtext>
+    <x-input-richtext name="body" :value="old('body' , $data['body'])"></x-input-richtext>
     <x-input-error :messages="$errors->get('body')" class="mt-2" />
 </div>
