c3f7a07fb586f1682b95bdc83929766cdc013ea3
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-06-02 15:09:30 -0300

    Componente radio e  atualizaçoes em _schedule

--- resources/views/campaigns/create/_schedule.blade.php
+++ resources/views/campaigns/create/_schedule.blade.php
@@ -1,4 +1,19 @@
-<div class="grid grid-cols-2 gap-4">
+<div class="flex flex-col gap-4">
+    <x-alert success  :title="__('Your campaign is ready to be send!')" />
+
+    <div>
+
+        <div>De:------@------</div>
+        <div>Para:</div>
+        <div>Assunto:{{$data['subject']}}</div>
+        <div>Template:</div>
+        
+    </div>
+    <hr>
+    <div>
+        <x-radio  id="send_now" name="send_when" value="now">{{__('Send now')}}</x-radio>
+        <x-radio id="send_later" name="send_when" value="later">{{__('Schedule delivery')}}</x-radio>
+    </div>
     <div>
         <x-input-label for="send_at" :value="__('Send_at')" />
         <x-text-input  id="send_at" class="block mt-1 w-full" type="date" name="send_at" :value="old('send_at', $data['send_at'])" autofocus />
